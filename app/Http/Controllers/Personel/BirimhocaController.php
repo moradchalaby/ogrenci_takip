1baf2798f691bd6f1a206016526900eb108fa35c
moradchalaby <moradchalaby@gmail.com>
2022-04-20 12:30:05 +0300

    gr

--- app/Http/Controllers/Personel/BirimhocaController.php
+++ app/Http/Controllers/Personel/BirimhocaController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use App\Models\Birimhoca;
 
-use Datatables;
+use \Yajra\Datatables\Datatables;
 
 class BirimhocaController extends Controller
 {
