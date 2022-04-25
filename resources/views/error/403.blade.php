728bc611aae7c946255e3643dc12f10340892f6e
moradchalaby <moradchalaby@gmail.com>
2022-04-25 16:23:10 +0300

    zx

---
+++ resources/views/error/403.blade.php
@@ -0,0 +1,14 @@
+@extends('errors::illustrated-layout')
+
+@section('code', '404 😭')
+
+@section('title', __('Page Not Found'))
+
+@section('image')
+
+    <div style="background-image: url('/images/404-bg.jpg');" class="absolute pin bg-no-repeat md:bg-left lg:bg-center">
+    </div>
+
+@endsection
+
+@section('message', __('Sorry, the page you are looking for could not be found.'))
--- routes/auth.php
+++ routes/auth.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    Route::get('eysnhsmo', [RegisteredUserController::class, 'create'])
         ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
