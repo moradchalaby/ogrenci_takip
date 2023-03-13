56dd307335ca012fc57bf85ffa8f1094f611c552
Murat Çelebi <moradchalaby@gmail.com>
2023-03-13 16:47:49 +0300

    sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{ route('routes.index') }}" class="brand-link">
         <img src="/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">AKMESCİD</span>
@@ -38,7 +38,7 @@
                 data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                        with font-awesome or any other icon font library -->
-                <li class="nav-header">MISCELLANEOUS</li>
+                <li class="nav-header">AKMESCİD</li>
                 <li class="nav-item">
                     <a href="/takvim" class="nav-link">
                         <i class="nav-icon fas fa-home"></i>
