bbde40608bdca4ef19c3bbd274f1b14e6b79bfa7
orinch <49117869+moradchalaby@users.noreply.github.com>
2023-03-13 16:14:25 +0300

    ssl disable

--- config/mail.php
+++ config/mail.php
@@ -44,6 +44,11 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true,
+            ],
         ],
 
         'ses' => [
