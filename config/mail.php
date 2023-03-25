e412dc2510b7064952ee4bcdeb67042be9caafee
orinch <49117869+moradchalaby@users.noreply.github.com>
2023-03-25 10:55:26 +0300

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -40,6 +40,7 @@ return [
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
+<<<<<<< Updated upstream
             'from' => ['address' => 'yaziliminyuzaki@gmail.com', 'name' => 'Akmescid Erkek Öğrenci Yurdu'],
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
@@ -49,6 +50,19 @@ return [
                 'verify_peer_name' => false,
                 'allow_self_signed' => true,
             ],]
+=======
+            'from' => ['address' => 'noreply@akmescid.com', 'name' => 'Akmescid Erkek Öğrenci Yurdu'],
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'auth_mode' => null,
+            'stream' => [
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ],
+>>>>>>> Stashed changes
         ],
 
         'ses' => [
